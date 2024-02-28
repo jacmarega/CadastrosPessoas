1e4554eacc0528651316e995d37fdaf78af10862
jacmarega <joaoaugustocomossato@gmail.com>
2024-02-28 16:31:31 -0400

    Nova

---
+++ CadastrosPessoas/Controllers/PessoaController.cs
@@ -0,0 +1,27 @@
+﻿using CadastrosPessoas.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadastrosPessoas.Controllers
+{
+    public class PessoaController : Controller
+    {
+        
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CadastrarPost(PessoaViewModel dados)
+        {
+            string nome = dados.Nome;
+            DateTime data = dados.DataNascimento;
+
+            return RedirectToAction("Cadastrar");
+        }
+    }
+
+}
---
+++ CadastrosPessoas/ViewModels/PessoaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadastrosPessoas.ViewModels
+{
+    public class PessoaViewModel
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string EstadoCivil { get; set; }
+        public string CPF { get; set; }
+        public string CEP { get; set; }
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+    }
+}
